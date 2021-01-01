6fbadfd474884cb17afc69cb09a79e9225f44a51
Mukesh Maurya <mukesh.india9@gmail.com>
2021-01-02 02:53:33 +0530

    initial boiler-plate & npm packages

---
+++ twitter-clone/src/Routing.js
@@ -0,0 +1,8 @@
+const Routing = () => {
+    return (
+        <div>
+            <h1>Hello Twitter</h1>
+        </div>
+    )
+};
+export default Routing;
---
+++ twitter-clone/src/Twitter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Routing from './Routing';
+
+class TwitterClone extends Component {
+
+    render() { 
+        return ( 
+            <Routing/>
+         );
+  
+  }
+}
+ 
+export default TwitterClone;
